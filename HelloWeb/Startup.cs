a9bf2230b5fcdcecbfee0194d275018b2fe1d813
nick hornung <nphornung77@gmail.com>
2020-04-14 20:09:15 -0500

    Razor Pages - intro, with error page test

--- HelloWeb/Startup.cs
+++ HelloWeb/Startup.cs
@@ -24,7 +24,7 @@ namespace HelloWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) // can replace with FALSE to test error page
             {
                 app.UseDeveloperExceptionPage();
             }
